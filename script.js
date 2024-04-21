bdd9388c3fd2b9730fb48bd80dc2c7679e39f8ad
Kaan <kaanasutay10@gmail.com>
2024-04-21 19:34:48 +0300

    create notesContainer.addEventListener

--- script.js
+++ script.js
@@ -3,7 +3,9 @@ const createBtn = document.querySelector(".btn");
 
 let notes = document.querySelectorAll(".input-box")
 
-
+function updateStorage() {
+    localStorage.setItem("notes", notesContainer.innerHTML);
+}
 
 createBtn.addEventListener("click", () => {
     let inputBox = document.createElement("p");
@@ -17,5 +19,14 @@ createBtn.addEventListener("click", () => {
 notesContainer.addEventListener("click", function(e) {
     if (e.target.tagName === "IMG") {
         e.target.parentElement.remove();
+        updateStorage();
+    }
+    else if (e.target.tagName === "p") {
+        notes = document.querySelectorAll(".input-box")
+        notes.forEach(nt => {
+            nt.onkeyup = function(){
+                updateStorage();
+            }
+        })
     }
 })
